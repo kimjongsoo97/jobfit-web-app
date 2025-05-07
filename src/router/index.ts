e733169cd40b9aba9fd78c0004b71a3fe2ab3460
Park JungHyun <jhyunpark0729@gmail.com>
2025-05-07 12:55:13 +0900

    feat: Navbar 인증 시 버튼 변경 기능 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -94,7 +94,7 @@ const router = createRouter({
       path: '/challenge',
       name: 'challenge',
       component: ChallengeView,
-      // beforeEnter: isAuthenticated,
+      beforeEnter: isAuthenticated,
       children: [
         {
           path: '',
